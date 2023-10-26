cfc204110c3ee1ac765a4166ddd60a4c28996208
Kosntantin Reznikov <reznikov.kosta@yandex.ru>
2023-10-26 19:28:24 +0300

    #1 - new's card fix

--- pages/card-new-preview.vue
+++ pages/card-new-preview.vue
@@ -1,18 +1,18 @@
 <template>
   <div>
-    <CardNew :article="{ category: categories.ADVS, title: 'Скоро собрание кафедры', date: '01.09.2023 в 12:00' }" />
+    <ArticleCard :article="{ category: categories.ADVS, title: 'Скоро собрание кафедры', date: '01.09.2023 в 12:00' }" />
     <br>
-    <CardNew :article="{ category: categories.NEWS, title: 'Открылся музей', cover: 'https://www.planetware.com/wpimages/2020/02/france-in-pictures-beautiful-places-to-photograph-eiffel-tower.jpg', date: '01.09.2023 в 13:00' }" />
+    <ArticleCard :article="{ category: categories.NEWS, title: 'Открылся музей', cover: 'https://www.planetware.com/wpimages/2020/02/france-in-pictures-beautiful-places-to-photograph-eiffel-tower.jpg', date: '01.09.2023 в 13:00' }" />
   </div>
 </template>
 
 <script>
-import CardNew, { CATEGORIES } from '../components/CardNew.vue';
+import ArticleCard, { CATEGORIES } from '../components/ArticleCard.vue';
 
 export default {
   name: 'CardNewPreviewPage',
   components: {
-    CardNew
+    ArticleCard
   },
   computed: {
     categories () {
