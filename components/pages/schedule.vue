12fc5f853f657ace858f14cd7b53f2cb6966eaf6
ShiryaevNikolay <shiryaev@surfstudio.ru>
2023-11-22 12:10:03 +0300

    #11 - удаляются моковые данные

--- components/pages/schedule.vue
+++ components/pages/schedule.vue
@@ -30,47 +30,7 @@ export default {
   props: {
     schedule: {
       type: Array,
-      default: () => [
-        {
-          everyWeek: true,
-          onlyNumerator: false,
-          onlyDenominator: false,
-          time: "8:00",
-          day: "Понедельник",
-          name: "Матеша",
-          classroom: 512,
-          teacher: {
-            surname: "Иванов"
-          },
-          color: "yellow"
-        },
-        {
-          everyWeek: false,
-          onlyNumerator: true,
-          onlyDenominator: false,
-          time: "9:45",
-          day: "Вторник",
-          name: "Матеша",
-          classroom: 12,
-          teacher: {
-            surname: "Иванова"
-          },
-          color: "green"
-        },
-        {
-          everyWeek: false,
-          onlyNumerator: false,
-          onlyDenominator: true,
-          time: "13:25",
-          day: "Среда",
-          name: "Физика",
-          classroom: 212,
-          teacher: {
-            surname: "Иванова"
-          },
-          color: "green"
-        }
-      ]
+      default: () => []
     }
   },
   computed: {
