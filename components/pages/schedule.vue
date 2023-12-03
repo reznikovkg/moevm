78a6005019f357edb05948f39b9273082a4029c9
Shiryaev Nikolay <deendeeps@list.ru>
2023-12-03 19:22:36 +0300

    Страница расписания (#13)
    * #11 - заполнение сетки grid
    
    * #11 - отображение занятий
    
    * #11 - стилизация рамок
    
    * #11 - удаляются моковые данные
    
    * #11 - добавляется хранилище
    
    * #11 - убирается дубликат computed
    
    * #11 - удаляются логи
    
    * #11 - удаляются логи
    
    * #11 - убирается значение по умолчанию
    
    * #11 - добавляются мутации и экшены
    
    * #11 - путсая строка
    
    * #11 - цвет в формате HEX
    
    * #11 - цвет в формате HEX
    
    * #11 - дефолтные цвета занятий
    
    * #11 - сетка таблицы через gap
    
    * #11 - пустая строка
    
    * #11 - используются props
    
    * #11 - удаляются лишние стили
    
    * #11 - удаляется лишний стиль
    
    * #11 - таблица расписания как отдельный компонент
    
    * #11 - удаляется неиспользуемый импорт
    
    * #11 - вычисляемый стиль как computed
    
    * #11 - выражения оборачиваются в скобки
    
    * #11 - правки ревью
    
    * #11 - цвет фона переносится в DayCard
    
    * #11 - правки стиля
    
    * #11 - меняются стили
    
    * #11 - меняется название класса
    
    * #11 - пропсы объектным стилем
    
    ---------
    
    Co-authored-by: ShiryaevNikolay <shiryaev@surfstudio.ru>

---
+++ components/pages/schedule.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <ScheduleTable :schedule="schedule"/>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from "vuex"
+import ScheduleTable from "../ScheduleTable.vue";
+
+export default {
+  name: "SchedulePage",
+  components: {
+    ScheduleTable
+  },
+  computed: {
+    ...mapGetters('schedule', {
+      schedule: "getSchedule"
+    })
+  }
+}
+</script>
