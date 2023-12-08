dab154f26c43ce4e7a91583826e59c816e7ff71d
Vladislav Kotov <vlad.kotov1999@ya.ru>
2023-12-08 19:22:16 +0300

    Сделать шапку сайта (#14)
    * [issue 3] Add assets
    
    * [issue 3] Add cards
    
    * [Issue 6] Add gray mode on click on cards
    
    * [Issue 6] Remove NavBar and duplicate svg files
    
    * [Issue 6] Align class names with BEM-style
    
    * [Issue 3] Revert changes
    
    * [issue 3] Remove storage for theme, add svg icons as class
    
    * [issue 3] Fix style
    
    * [issue 3] revert changes
    
    * [issue 3] Remove extra lines
    
    * [issue 3] Remove svg

---
+++ components/CategoryCard.vue
@@ -0,0 +1,87 @@
+<template>
+  <div v-if="category" class="category-card">
+    <div class="category-card__wrapper">
+      <div class="category-card__header" :style="style">
+        <h3 class=" category-card__title">
+          {{ category.title }}
+        </h3>
+      </div>
+      <div class="category-card__content">
+        <div class="category-card__list">
+          <span v-if="expanded">
+            <div v-for="(item, index) in category.content" :key="index">
+              <nuxt-link :to="{ name: item }"> {{ item }}</nuxt-link>
+            </div>
+          </span>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+
+export default {
+  name: 'CategoryCard',
+  props: {
+    items: {
+      default: false,
+      type: Boolean
+    },
+    category: {
+      default: () => (null),
+      type: Object
+    },
+    expanded: {
+      default: false,
+      type: Boolean
+    }
+  },
+  computed: {
+    style () {
+      if (!this.category) {
+        return {}
+      }
+      if (this.expanded) {
+        return {
+          height: '70px',
+          backgroundImage: `url(${this.category.image})`
+        }
+      } else {
+        return {
+          height: null,
+          backgroundImage: `url(${this.category.image})`
+        }
+      }
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.category-card {
+  width: 100%;
+
+  &__wrapper {
+    display: flex;
+    flex-direction: column;
+    gap: 14px;
+  }
+
+  &__header {
+    text-align: center;
+    align-items: center;
+    background: #bababa;
+    background-size: cover;
+    background-position: center;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-grow: 1;
+  }
+
+  &__title {
+    margin: 0;
+    color: white;
+  }
+}
+</style>
---
+++ components/main/page/Header.vue
@@ -0,0 +1,146 @@
+<template>
+  <header class="header" :style="styleHeader">
+    <div class="header__line">
+      {{ title }}
+    </div>
+    <div class="header__icons main-container">
+      <div class="header__title">
+        <img src="@/assets/pictures/computer.png" class="header__logo" />
+        <h3 class="header__text">Кафедра Математического обеспечения ЭВМ</h3>
+      </div>
+      <div class="header__buttons">
+        <input v-model="value" type="search" autocomplete="off" placeholder="Введите" class="header__form" />
+        <SearchIcon :color="styleIcon" />
+        <MenuIcon :color="styleIcon" @click.native="() => openList()" />
+      </div>
+    </div>
+    <div class="header__cards main-container">
+      <CategoryCard
+        :category="{ title: 'Наука', image: 'https://ui.pnzreg.ru/upload/iblock/c94/3016425.jpg', content: ['Пункт 1', 'Пункт 2', 'Пункт 3', 'Пункт 4', 'Пункт 5'] }"
+        :expanded="clicked"
+        />
+      <CategoryCard
+        :category="{ title: 'Образование', image: 'https://ui.pnzreg.ru/upload/iblock/c94/3016425.jpg', content: ['Пункт 1', 'Пункт 2', 'Пункт 3', 'Пункт 4', 'Пункт 5'] }"
+        :expanded="clicked"
+        />
+      <CategoryCard
+        :category="{ title: 'Структура', image: 'https://ui.pnzreg.ru/upload/iblock/c94/3016425.jpg', content: ['Пункт 1', 'Пункт 2', 'Пункт 3', 'Пункт 4', 'Пункт 5'] }"
+        :expanded="clicked"
+        />
+      <CategoryCard
+        :category="{ title: 'Прочее', image: 'https://ui.pnzreg.ru/upload/iblock/c94/3016425.jpg', content: ['Пункт 1', 'Пункт 2', 'Пункт 3', 'Пункт 4', 'Пункт 5'] }"
+        :expanded="clicked"
+        />
+    </div>
+  </header>
+</template>
+
+<script>
+import CategoryCard from '@/components/CategoryCard.vue'
+import SearchIcon from '@/components/SearchIcon.vue'
+import MenuIcon from '@/components/MenuIcon.vue'
+
+export default {
+  name: "Header",
+  components: {
+    CategoryCard,
+    SearchIcon,
+    MenuIcon
+  },
+  props: {
+    value: {
+      type: String,
+      required: false
+    }
+  },
+  data () {
+    return {
+      clicked: false,
+      title: 'Главная'
+    };
+  },
+  computed: {
+    styleHeader () {
+      if (this.clicked) {
+        return { color: '#ebf4f1', 'background-color': '#5e5f61' }
+      }
+      else {
+        return { color: '#243746', 'background-color': 'white' }
+      }
+    },
+    styleIcon () {
+      return this.clicked ? 'white' : 'black'
+    }
+  },
+  methods: {
+    openList () {
+      this.clicked = !this.clicked
+      if (this.title === 'Главная') {
+        this.title = 'Меню'
+      }
+      else {
+        this.title = 'Главная'
+      }
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.header {
+
+  &__line {
+    font-weight: 700;
+    opacity: 1;
+    color: #000;
+    text-decoration: none;
+    background-color: rgb(68, 64, 64);
+    height: 20px;
+    margin: 0 auto;
+    display: flex;
+  }
+
+  &__icons {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+
+  &__title {
+    display: flex;
+  }
+
+  &__logo {
+    max-height: 120px;
+  }
+
+  &__buttons {
+    display: flex;
+  }
+
+  &__form {
+    border-style: solid;
+    border-color: #ccc;
+    border-width: 2px;
+    border-bottom: 1;
+    border-left: 0;
+    border-right: 0;
+    border-top: 0;
+  }
+
+  &__text {
+    padding-left: 20px;
+    word-wrap: break-word;
+    max-width: 250px;
+    vertical-align: middle;
+  }
+
+  &__cards {
+    display: flex;
+    flex-direction: row;
+    width: 100%;
+    justify-content: space-between;
+    gap: 5px;
+  }
+}
+</style>
